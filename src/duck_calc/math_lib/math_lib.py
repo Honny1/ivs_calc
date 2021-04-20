b54f269e76b095bc9c6bafbbcad9bb28beceb11d
Tom Szabo <tomszabo21@seznam.cz>
2021-04-20 12:25:10 -0700

    Refaktorovani nekterych funkci na privatni a oprava vypoctu odmocniny

--- src/duck_calc/math_lib/math_lib.py
+++ src/duck_calc/math_lib/math_lib.py
@@ -58,134 +58,13 @@ class MathLib():
         except KeyError:
             return False
 
-    """Clear stack"""
-    def _clear_stack(self):
-        while self.top_index:
-            self._pop()
-
-    """Translate equation input string to postfix"""
-    @staticmethod
-    def transform_string_to_postfix(input_string):
-        stack = MathLib()
-
-        if " " in input_string:
-            input_string = input_string.replace(" ", "")
-        len_of_input = len(input_string)
-
-        # Iterate input
-        i = 0
-        while i < len_of_input:
-            operator = input_string[i]
-            # Num
-            if operator.isdigit():
-                i = stack._load_num(input_string, i, len_of_input)
-                if i is None:
-                    return None
-                continue
-            # Operation
-            elif operator == "-":
-                if i == 0 or not \
-                    MathLib.isNumber(input_string[i-1]) and \
-                        input_string[i-1] not in ")(":
-                    stack.output += "-"
-                    i += 1
-                    i = stack._load_num(input_string, i, len_of_input)
-                    if i is None:
-                        return None
-                    continue
-                else:
-                    # Fill string with operator
-                    is_greater = stack._not_greater(operator)
-                    if is_greater is None:
-                        return None
-                    while (MathLib.stackShouldPop(stack, operator)):
-                        stack.output += " " + stack._pop()
-                    stack._push(operator)
-            # Sin
-            elif operator == "s":
-                i = MathLib.evaluate_sin_and_cos(input_string, i, stack, "sin")
-                if i is None:
-                    return None
-            # Cos
-            elif operator == "c":
-                i = MathLib.evaluate_sin_and_cos(input_string, i, stack, "cos")
-                if i is None:
-                    return None
-            # Normal operators
-            elif operator in "+/*^_":
-                # Fill string with operators
-                while (MathLib.stackShouldPop(stack, operator)):
-                    stack.output += " " + stack._pop()
-                stack._push(operator)
-            else:
-                return None
-
-            stack.output += " "
-            i += 1
-
-        # Empty stack
-        if stack.output[len(stack.output)-1] != " ":
-            stack.output += " "
-        while stack.top_index != 0:
-            stack.output += str(stack._pop()) + " "
-
-        stack.output = re.sub(r'\ (?=\ )', '', stack.output)
-        return stack.output[0:-1]
-
-    """Translate equation input string to postfix"""
-    @staticmethod
-    def solve_mathematic_problem(input_string):
-        postfix_string = MathLib.transform_string_to_postfix(input_string)
-        if postfix_string is None:
-            return None
-        return MathLib.solve_postfix_equation(postfix_string)
-
-    """Eval input postfix to number"""
-    @staticmethod
-    def solve_postfix_equation(input_postfix):
-        stack = MathLib()
-        result = ""
-
-        for element in input_postfix.split():
-            if MathLib.isNumber(element):
-                stack._push(element)
-            else:
-                if element == "sin" or element == "cos":
-                    val1 = stack._pop()
-                    # Checks None
-                    if val1 is None:
-                        return None
-                    result = str(eval("math." + element + "(" + val1 + ")"))
-                elif element in "+/*^_-":
-                    val1 = stack._pop()
-                    val2 = stack._pop()
-
-                    # Checks None
-                    if val1 is None or val2 is None:
-                        return None
-
-                    if element == "^":
-                        result = str(eval(val2 + "**" + val1))
-
-                    elif element == "_":
-                        # root
-                        if float(val2) <= 0:
-                            return None
-                        result = str(eval(val2 + "**(1/" + val1 + ")"))
-
-                    else:
-                        # Checks dividing by zero
-                        if element == "/":
-                            if val1 == "0":
-                                return None
-                            if val2 == "0":
-                                stack._push(val2)
-                                continue
-                        result = str(eval(val2 + element + val1))
-                else:
-                    return None
-                stack._push(result)
-        return float(stack._pop())
+    """Defines the situation if stack should pop an operation to output"""
+    def _stackShouldPop(self, operator):
+        return self.top_index != 0 \
+            and (self._not_greater(operator)
+                 and self._left_assoc(operator)
+                 or not self._left_assoc(operator)
+                 and self._lesser(operator))
 
     """Goes through input_string and gets whole number"""
     def _load_num(self, input_string, i, len_of_input):
@@ -222,20 +101,136 @@ class MathLib():
         return i
 
     """Defines the association when pow or root are in a row"""
-    def left_assoc(operator):
+    def _left_assoc(self, operator):
         return operator != "^" and operator != "_"
 
     """Checks if number is valid via regex"""
-    def isNumber(str):
+    def _isNumber(self, str):
         return re.search("^-?[0-9]+(.[0-9]+)?$", str)
 
-    """Defines the situation if stack should pop an operation to output"""
-    def stackShouldPop(stack, operator):
-        return stack.top_index != 0 \
-            and (stack._not_greater(operator)
-                 and MathLib.left_assoc(operator)
-                 or not MathLib.left_assoc(operator)
-                 and stack._lesser(operator))
+    """Translate equation input string to postfix"""
+    @staticmethod
+    def transform_string_to_postfix(input_string):
+        stack = MathLib()
+
+        if " " in input_string:
+            input_string = input_string.replace(" ", "")
+        len_of_input = len(input_string)
+
+        # Iterate input
+        i = 0
+        while i < len_of_input:
+            operator = input_string[i]
+            # Num
+            if operator.isdigit():
+                i = stack._load_num(input_string, i, len_of_input)
+                if i is None:
+                    return None
+                continue
+            # Operation
+            elif operator == "-":
+                if i == 0 or not \
+                    stack._isNumber(input_string[i-1]) and \
+                        input_string[i-1] not in ")(":
+                    stack.output += "-"
+                    i += 1
+                    i = stack._load_num(input_string, i, len_of_input)
+                    if i is None:
+                        return None
+                    continue
+                else:
+                    # Fill string with operator
+                    is_greater = stack._not_greater(operator)
+                    if is_greater is None:
+                        return None
+                    while (stack._stackShouldPop(operator)):
+                        stack.output += " " + stack._pop()
+                    stack._push(operator)
+            # Sin
+            elif operator == "s":
+                i = MathLib.evaluate_sin_and_cos(input_string, i, stack, "sin")
+                if i is None:
+                    return None
+            # Cos
+            elif operator == "c":
+                i = MathLib.evaluate_sin_and_cos(input_string, i, stack, "cos")
+                if i is None:
+                    return None
+            # Normal operators
+            elif operator in "+/*^_":
+                # Fill string with operators
+                while (stack._stackShouldPop(operator)):
+                    stack.output += " " + stack._pop()
+                stack._push(operator)
+            else:
+                return None
+
+            stack.output += " "
+            i += 1
+
+        # Empty stack
+        if stack.output[len(stack.output)-1] != " ":
+            stack.output += " "
+        while stack.top_index != 0:
+            stack.output += str(stack._pop()) + " "
+
+        stack.output = re.sub(r'\ (?=\ )', '', stack.output)
+        return stack.output[0:-1]
+
+    """Translate equation input string to postfix"""
+    @staticmethod
+    def solve_mathematic_problem(input_string):
+        postfix_string = MathLib.transform_string_to_postfix(input_string)
+        if postfix_string is None:
+            return None
+        return MathLib.solve_postfix_equation(postfix_string)
+
+    """Eval input postfix to number"""
+    @staticmethod
+    def solve_postfix_equation(input_postfix):
+        stack = MathLib()
+        result = ""
+
+        for element in input_postfix.split():
+            if stack._isNumber(element):
+                stack._push(element)
+            else:
+                if element == "sin" or element == "cos":
+                    val1 = stack._pop()
+                    # Checks None
+                    if val1 is None:
+                        return None
+                    result = str(eval("math." + element + "(" + val1 + ")"))
+                elif element in "+/*^_-":
+                    val1 = stack._pop()
+                    val2 = stack._pop()
+
+                    # Checks None
+                    if val1 is None or val2 is None:
+                        return None
+
+                    if element == "^":
+                        result = str(eval(val2 + "**" + val1))
+
+                    elif element == "_":
+                        # root
+                        if float(val2) < 0:
+                            return None
+                        result = str(eval(val2 + "**(1/" + val1 + ")"))
+
+                    else:
+                        # Checks dividing by zero
+                        if element == "/":
+                            if val1 == "0":
+                                return None
+                            if val2 == "0":
+                                stack._push(val2)
+                                continue
+                        result = str(eval(val2 + element + val1))
+                else:
+                    return None
+                stack._push(result)
+        return float(stack._pop())
 
     """Makes number from sin and cos"""
     @staticmethod
@@ -256,8 +251,6 @@ class MathLib():
             i += 1
         result += MathLib.transform_string_to_postfix(in_braces)
 
-        # if len(stack.output):
-        #    stack.output += " "
         stack.output += str(result) + " " + stringType
 
         return i
