8076d8c7eb1c2fc08cb3882bc17803319f1bba42
Jan Rodak <hony.com@seznam.cz>
2021-04-25 13:24:01 +0200

    1.0.1

--- src/build-clean.sh
+++ src/build-clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-version="1.0.0"
+version="1.0.1"
 # Odstrani nepotrebne soubory po buildu
 rm -rf duck-calc-${version}.tar.gz duck-calc-${version}/ duck-calc_${version}-1.debian.tar.xz duck-calc_${version}-1.dsc duck-calc_${version}-1_amd64.build duck-calc_${version}-1_amd64.buildinfo duck-calc_${version}-1_amd64.changes duck-calc_${version}.orig.tar.gz
